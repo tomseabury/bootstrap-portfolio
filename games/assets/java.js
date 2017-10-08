db0aad600d1b880edadc09aeaeac1a4af6cb0ff1
Thomas <tommy.seabury@gmail.com>
2017-10-07 23:38:22 -0600

    Hangman Game

---
+++ games/assets/java.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+//174 words
+var wordBank = ["afraid","afterlife","alarming","alien","angel","apparition","astronaut","autumn","ballerina","bat","beast","bizarre","black","blood","bloodcurdling","bogeyman","bone","boo","broomstick","cackle","cadaver","candy","cape","carve","casket","cat","cauldron","cemetery","chilling","cloak","clown","cobweb","coffin","corpse","costume","creepy","crown","crypt","dark","darkness","dead","demon","devil","devilish","disguise","dreadful","death","eerie","elf","enchant","evil","eyeballs","eyepatch","fairy","fangs","fear","flashlight","fog","fright","frighten","frightening","frightful","genie","ghastly","ghost","ghostly","ghoul","ghoulish","goblin","goodies","gory","gown","grave","gravestone","grim","grisly","gruesome","Halloween","hat","haunt","hayride","headstone","hobgoblin","horrible","horrify","howl","imp","jumpsuit","lantern","magic","mask","masquerade","mausoleum","midnight","mist","monster","moon","moonlight","moonlit","morbid","mummy","mysterious","night","nightmare","October","ogre","orange","otherworldly","owl","party","petrify","phantasm","phantom","pirate","pitchfork","poltergeist","potion","prank","pretend","pumpkin","repulsive","revolting","RIP","robe","robot","scare","scarecrow","scary","scream","shadow","shadowy","shock","shocking","skeleton","skull","soldier","specter","spell","spider","spirit","spook","spooky","startling","strange","superhero","supernatural","superstition","sweets","tarantula","terrible","terrify","thrilling","tomb","tombstone","treat","treats","trick","troll","unearthly","unnerving","vampire","vanish","wand","warlock","web","werewolf","wicked","wig","witch","witchcraft","wizard","wizardry","wraith","zombie"]
+var randomWord
+var letterArray
+var wins = 0
+var guesses = 10
+var correct = 0
+var winCheck = true
+// $("#random-button").on("click", function(){
+// 	var lottoNumber = ""
+// 	for (i=0; i<9; i++){
+// 	var random = Math.floor(Math.random()*10);
+// 	lottoNumber = lottoNumber + random.toString()
+// 	}
+// 	$("#random-number").prepend("<li>" + lottoNumber + "</li>");
+// })
+
+
+function generateWord(){
+	randomWord = wordBank[Math.floor(Math.random()*174)];
+	letterArray= randomWord.toUpperCase().split("");
+	letterArray.forEach(function(g){
+		$("#correctLetters").append('<div class= "' + g + '" style= "float: left; padding-right: 10px;">__</div>');
+	})
+}
+
+
+
+document.onkeyup = function(event) {
+if (guesses > 1 && winCheck === true) {
+var letter = String.fromCharCode(event.keyCode).toUpperCase();
+
+if (letterArray.indexOf(letter) > -1) {
+	$("."+letter).text(letter);
+	correct = correct + 1
+	if (correct > letterArray.length - 1) {
+	// this is the win condition, but it does not work if the
+	// word has a letter that appears more than once in it
+		$("h2").text("You did it!!")
+		wins = wins + 1
+		winCheck = false
+	}
+
+}else{
+	$("#guessedLetters").append(letter);
+	guesses = guesses - 1
+	$("h4").text("You can only miss " + guesses + " more!")
+}
+}else if (guesses < 2){
+	$("h4").text("Oh no! Looks like you have ran out of guesses..")
+	// The assignment says to automatically pick a new word and start
+	// over, I didn't like that as much, but here is the code to do it.
+	// $("#correctLetters").empty();
+	// $("#guessedLetters").empty();
+	// $("h2").text("Words correctly guessed: " + wins);
+	// $("h4").text("You can only miss 10 letters!");
+	// correct = 0
+	// guesses = 10
+	// winCheck = true
+	// generateWord();
+}
+
+};
+
+
+$(".btn").hover(function(){
+	$(this).animate({opacity: "1"});
+	}, function(){
+	$(this).animate({opacity: ".2"});
+});
+
+$("h2").hover(function(){
+	$(this).animate({opacity: "1"});
+	}, function(){
+	$(this).animate({opacity: ".2"}, "slow");
+});
+
+$(".btn").on("click",function(){
+	$("#correctLetters").empty();
+	$("#guessedLetters").empty();
+	$("h2").text("Words correctly guessed: " + wins);
+	$("h4").text("You can only miss 10 letters!");
+	correct = 0
+	guesses = 10
+	winCheck = true
+	generateWord();
+});
+
+
+
+
+
+
+generateWord();
+});
