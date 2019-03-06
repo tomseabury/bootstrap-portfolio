d3dae38a60b14aa507998900f48adcf9a4e6ce1a
Thomass <thomas.seabury@tazworks.com>
2019-03-06 15:24:31 -0700

    Testing

--- games/adventuresAbroad/javaScript.js
+++ games/adventuresAbroad/javaScript.js
@@ -204,23 +204,23 @@ $(()=> {
     myMap();
     setBackGround();
     
-    // $("tr").on('click', function (){
-    //     if(this.id != ""){
-    //         var geocoder = new google.maps.Geocoder
-    //         var rowID = this.id.substr(this.id.length - 1);
-    //         geocoder.geocode({
-    //             address: infoArr[rowID].details.address,
-    //         }, function(results,status){
-    //             if (status == google.maps.GeocoderStatus.OK) {
-    //                 map.setCenter(results[0].geometry.location);
-    //                 map.setZoom(10);
-    //                 document.body.style.backgroundImage = "url("+infoArr[rowID].details.image+")"
-    //               } else {
-    //                 alert('Geocode was not successful for the following reason: ' + status);
-    //               };
-    //         });
-    //     };
-    // });
+    $("tr").on('click', function (){
+        if(this.id != ""){
+            var geocoder = new google.maps.Geocoder
+            var rowID = this.id.substr(this.id.length - 1);
+            geocoder.geocode({
+                address: infoArr[rowID].details.address,
+            }, function(results,status){
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                    map.setZoom(10);
+                    document.body.style.backgroundImage = "url("+infoArr[rowID].details.image+")"
+                  } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                  };
+            });
+        };
+    });
 
     function goToAddress(rowID){
         var geocoder = new google.maps.Geocoder;
