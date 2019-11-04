bb0c93d4bc6605be104385245da267af8e0afccc
Thomas <tommy.seabury@gmail.com>
2019-11-03 19:30:23 -0700

    connected firebase DB and removed location array

--- games/adventuresAbroad/javaScript.js
+++ games/adventuresAbroad/javaScript.js
@@ -19,7 +19,7 @@ $(()=> {
 
     database.ref("/locations").on("child_added", function(snapShot){
         
-        console.log("Hellow world");
+        console.log("Hello world");
         console.log(snapShot.val());
         createTable(snapShot);
         $("tr").on('click', function (){
@@ -56,90 +56,16 @@ $(()=> {
         }}
     ];
 
-    var infoArr = [
-        {details:{
-            name: "Yellowstone",
-            address: "Yellowstone National Park, WY 82190",
-            travleInfo:{
-                milesFromHome: 335,
-                timeToDestination: "4 h 59 min",
-            },
-            image: "http://foundtheworld.com/wp-content/uploads/2015/12/Yellowstone-National-Park-11.jpg"}
-        },
-        {details:{
-            name: "Strawberry Reservoir",
-            address: "Forest Rd 629, Heber City, UT 84032",
-            travleInfo:{
-                milesFromHome: 82,
-                timeToDestination: "1 h 39 min",
-            },
-            image: "https://media.deseretdigital.com/file/9a84db1767?crop=top:0|left:0|width:1260|height:670|gravity:Center&quality=55&interlace=none&resize=width:1260&order=resize,crop&c=14&a=e0f131f0"}
-        },
-        {details:{
-            name: "Bear Lake",
-            address: "Rendezvous Beach Rd, Laketown, UT 84038",
-            travleInfo:{
-                milesFromHome: 142,
-                timeToDestination: "2 h 23 min",
-            },
-            image: "https://i2.wp.com/www.spendlifetraveling.com/wp-content/uploads/2016/10/big_bear_lake_travel_tips.jpg?fit=1600%2C850&ssl=1"}
-        },
-        {details:{
-            name: "Zion National Park",
-            address: "171-163 Zion – Mount Carmel Hwy, Springdale, UT 84767",
-            travleInfo:{
-                milesFromHome: 297,
-                timeToDestination: "4 h 23 min",
-            },
-            image: "http://www.somethingsbrewing.com/wp-content/uploads/2011/09/Zion_HDR-3.jpg"}
-        },
-        {details:{
-            name: "Arches",
-            address: "Arches Scenic Dr, Moab, UT 84532",
-            travleInfo:{
-                milesFromHome: 218,
-                timeToDestination: "3 h 27 min",
-            },
-            image: "http://hdqwalls.com/wallpapers/arches-national-park-gm.jpg"}
-        },
-        {details:{
-            name: "Grand Canyon",
-            address: "Unnamed Road, North Rim, AZ 86052",
-            travleInfo:{
-                milesFromHome: 512,
-                timeToDestination: "8 h 10 min",
-            },
-            image: "https://media.deseretdigital.com/file/aa89988589?type=jpeg&quality=55&c=15&a=4379240d"}
-        },
-        {details:{
-            name: "Lake Powell",
-            address: "100 Wahweap Blvd, Page, AZ 86040",
-            travleInfo:{
-                milesFromHome: 412,
-                timeToDestination: "9 h 31 min",
-            },
-            image: "http://www.travelswithtracy.net/wp-content/uploads/2014/05/Alstrom-Point-7.jpg"}
-        },
-        {details:{
-            name: "Goblin Valley",
-            address: "Goblin Valley Rd, Green River, UT 84525",
-            travleInfo:{
-                milesFromHome: 202,
-                timeToDestination: "3 h 27 min",
-            },
-            image: "https://www.stgeorgeutah.com/wp-content/uploads/2018/04/Goblin-Valley-landscape.jpg"}
-        },
-        {details:{
-            name: "Havasupai",
-            address: "Havasupai, Supai, AZ 86435",
-            travleInfo:{
-                milesFromHome: 633,
-                timeToDestination: "9 h 45 min",
-            },
-            image: "https://www.coloradoavidgolfer.com/wp-content/uploads/2017/03/Havasupai-Falls.jpg"}
-        }
-    ];
 
+    function writeLocationData(location, name, email, imageUrl) {
+        firebase.database().ref('users/' + location).set({
+          username: name,
+          email: email,
+          profile_picture : imageUrl
+        });
+      }
+
+      writeLocationData
 
     function createTableNoDataBase(){
         var age;
@@ -222,24 +148,6 @@ $(()=> {
     setBackGround();
     createTableNoDataBase();
     
-    $("tr").on('click', function (){
-        if(this.id != ""){
-            var geocoder = new google.maps.Geocoder
-            var rowID = this.id.substr(this.id.length - 1);
-            geocoder.geocode({
-                address: this.value,
-            }, function(results,status){
-                if (status == google.maps.GeocoderStatus.OK) {
-                    map.setCenter(results[0].geometry.location);
-                    map.setZoom(10);
-                    document.body.style.backgroundImage = "url("+infoArr[rowID].details.image+")"
-                  } else {
-                    alert('Geocode was not successful for the following reason: ' + status);
-                  };
-            });
-        };
-    });
-
     $("h1").on('click', function(){
         map.setCenter({
             lat: 41.7102,
